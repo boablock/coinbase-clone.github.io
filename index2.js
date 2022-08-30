d45003588d2954f92c53e671985cf2be093ccd22
boablock <boablockchain@gmail.com>
2022-08-30 19:13:24 -0300

    branch-1

---
+++ index2.js
@@ -0,0 +1,67 @@
+
+
+
+
+/*** PASOS DEL CONVERSOR
+ * 1)Bienvenida y datos de usuario (funcion login) 
+ * 2)Ingresar moneda (USD/ARS) (funcion converet)
+ * 3)Ingresar criptomoneda a convertir
+ * 4)Ingresar monto en USD/ARS
+ * 5)Devolver resultado
+ */
+
+ let userAndPass ='';
+ function login (){ 
+     alert('Bienvenido a ConversorCripto')
+     let user = prompt('Ingrese su usuario')
+     let passWord = prompt('Ingrese su contraseña')
+     while (user === '' || passWord === ''){
+         user = prompt('Ingrese su usuario')
+         passWord = prompt('Ingrese su contraseña')
+     }
+     userAndPass = user + passWord
+     return userAndPass;
+ }
+ login();
+ // userAndPass = login(); // pregunta: si declaro userAndPass dentro de la funcion, y en el ambito global le asigno el valor d ela funcion, no me dice que no existe. 
+ console.log(userAndPass);
+
+ 
+
+ 
+
+ 
+ const precioBtc = 20000;
+ const precioEth = 2000;
+ const dolar = 300; 
+
+ function converter(){
+     let de;
+     let a; 
+     let monto; 
+     do{
+         de = parseInt(prompt('ingrese un numero segun sea su moneda: \n 1)USD 2)ARS'))
+         a = parseInt (prompt('Ingrese un numero segun la criptomoneda deseada: \n1)BTC \n2)ETH'));
+         monto = parseFloat( prompt('Ingrese el monto que desee convertir'));
+
+     } while ((de != 1 && de != 2) || (a !=1 && a != 2)); //--> pobrar con isNaN (para que repira el ciclo si monto no es numero)
+
+     if ( de === 1 && a === 1){
+         return ('$' + monto + ' = '+ monto / precioBtc + 'BTC');
+     }else if (de === 1 && a === 2){
+         return ('$' + monto + ' = '+ monto / precioEth + 'ETH');
+     } else if (de  === 2 && a === 1){
+         return ('$' + monto + ' = '+ monto / dolar / precioBtc + 'BTC');
+     }else if (de === 2 && a === 2){
+         return ('$' + monto + ' = '+ monto / dolar / precioEth + 'ETH');
+     }
+ }
+
+ // console.log (conversor()); // se usa console.log para ver el valor que tiene las variabels o estan trabajando las funciones
+ let converResult = converter();
+
+ console.log(converResult);
+
+ alert(converResult);
+
+     
