04e0111db55bd660d0ff06dbe2dff5a9d72d831f
boablock <boablockchain@gmail.com>
2022-09-07 20:33:07 -0300

    agregue el alert de la funcion converter

--- currency-converter/index.js
+++ currency-converter/index.js
@@ -1,13 +1,4 @@
-    
-    
-    
-      let precioBtc = 20000;
-      let precioEth = 2000;
-      let dolar = 300;
-  
-      let moneda;
-      let crypto; 
-      let monto;
+
 
     let option;
     const datosUsuario =[
@@ -22,7 +13,13 @@
 
     console.log(datosUsuario);
 
+    let precioBtc = 20000;
+    let precioEth = 2000;
+    let dolar = 300;
   
+    let moneda;
+    let crypto; 
+    let monto;
 
     while (option !== 4){
         option = Number(prompt('Ingrese una opcion:\n 1) Login/sing-up\n 2)Conversor crypto\n 3)Seccion favoritos\n 4) Salir'));
@@ -40,10 +37,8 @@
                 moneda = parseInt(prompt('ingrese un numero segun sea su moneda: \n 1)USD 2)ARS'));
                 crypto = parseInt(prompt('Ingrese un numero segun la criptomoneda deseada: \n1)BTC \n2)ETH'));
                 monto = parseInt(prompt('Ingrese el monto que desee convertir'));
-                converter(moneda, crypto, monto);
-                document.write(monto * precioBtc);
+                alert(converter(moneda, crypto, monto));
                 break;
-
             case 3:
                 alert('Ingresaste la opcion 3');
                 break;
@@ -55,7 +50,6 @@
         }
     }
 
-
     function newUser (nombre, mail, usuario, contraseña){
         datosUsuario.push({
             nombre,
@@ -65,26 +59,31 @@
         })
     }
     
-   
-
-    const suma = (a,b) => a / b; 
-    alert(suma(monto),precioBtc); 
-
-
 
     function converter(moneda, crypto, monto){
-        
-    if ( moneda === 1 && crypto === 1){
-        return ('$' + monto + ' = '+ monto / precioBtc + 'BTC');
-    }else if (moneda === 1 && crypto === 2){
-        return ('$' + monto + ' = '+ monto / precioEth + 'ETH');
-    } else if (moneda  === 2 && monto === 1){
-        return ('$' + monto + ' = '+ monto / dolar / precioBtc + 'BTC');
-    }else if (moneda === 2 && monto === 2){
-        return ('$' + monto + ' = '+ monto / dolar / precioEth + 'ETH');
-    }
+        if ( moneda === 1 && crypto === 1){
+            return ('$' + monto + ' = '+ monto / precioBtc + 'BTC');
+        }else if (moneda === 1 && crypto === 2){
+            return ('$' + monto + ' = '+ monto / precioEth + 'ETH');
+        } else if (moneda  === 2 && monto === 1){
+            return ('$' + monto + ' = '+ monto / dolar / precioBtc + 'BTC');
+        }else if (moneda === 2 && monto === 2){
+            return ('$' + monto + ' = '+ monto / dolar / precioEth + 'ETH');
+        }
     }
   
+  
+
+
+    // const suma = (a,b) => a / b; 
+    // alert(suma(monto),precioBtc); 
+
+    // function suma (a,b){
+    //     return a/b; 
+    // }
+
+    // console.log(suma(100,10));
+
 
 /*
     let userAndPass ='';
@@ -106,15 +105,6 @@
 
 
 
-
-    
-
-
-
-
-
-
- 
 //  const precioBtc = 20000;
 //  const precioEth = 2000;
 //  const dolar = 300; 
