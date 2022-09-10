a1aa6ba9165eacee454a8c1967fb4f8411c9465d
boablock <boablockchain@gmail.com>
2022-09-10 17:05:00 -0300

    .

--- currency-converter/index.js
+++ currency-converter/index.js
@@ -2,6 +2,7 @@
     // MODELO-AFTER
     
     // Array para recolectar los datos de cada usuario
+    
     const datosUsuarios =[
         {
             id:1,
@@ -20,12 +21,14 @@
             contraseña: 'lalal23',
         }
     ]
-    // Metodo de filtrado array (apellido)
-    const busqueda = datosUsuarios.filter(dato =>{
-        return dato.apellido  === 'Gonzales';
-        }
-    )
-    console.log(busqueda);
+    console.log(datosUsuarios);
+
+    // // Metodo de filtrado array (apellido)
+    // const busqueda = datosUsuarios.filter(dato =>{
+    //     return dato.apellido  === 'Gonzales';
+    //     }
+    // )
+    // console.log(busqueda);
 
     // Array para recolectar las conversiones de cada usuario
     const conversionesUsuarios =[
@@ -45,16 +48,23 @@
     console.log(conversionesUsuarios);
    
     //Array inversiones de usuarios
-    const tusInversiones =[
+    const userInvestments =[
         {}
     ]
-    console.log(tusInversiones);
+    console.log(userInvestments);
+
+    //Array monedas
+    const monedas=[
+        'USD',
+        'ARS',
+    ]
 
     //Array cryptomonedas
     const cryptomonedas =[
-        {
-
-        }
+        'BTC',
+        'ETH',
+        'ADA',
+        'BNB',
     ]
     
     let option;
@@ -66,16 +76,16 @@
     let monto;
     // Estrucura inicial, condicional base
     while (option !== 4){
-        option = Number(prompt('Ingrese una opcion:\n 1) Login/sing-up\n 2)Conversor crypto\n 3) Tus inversiones \n4) Salir'));
+        option = Number(prompt('Ingrese una opcion:\n 1) Login/sing-up\n 2)Conversor crypto\n 3) Seccion inversiones \n4) Salir'));
         switch(option){
             case 1:
                 const nombre = prompt('Ingresa tu nombre');
                 const mail = prompt('Ingresa tu mail');
                 const usuario = prompt('Ingresa tu usuario');
                 const contraseña =prompt ('Ingresa tu contraseña')
-                const id = getLastID() + 1;
+                const creationId = getLastID() + 1;
                 // const id = datosUsuarios.length + 1; //--> se haria de esta forma, pero lo hicimos con una funcion para abstraer y encapsular un comportamiento e invocarlo aca. 
-                newUser(nombre, mail, usuario, contraseña, id);
+                newUser(nombre, mail, usuario, contraseña, creationId);
                 alert('Bienvenido ' + nombre + 'ID: '+ id);
                 break;
             case 2:
@@ -88,11 +98,17 @@
            
             case 3:
                 alert('Bienvenido a la seccion inversiones')
-                cryptoComprada = prompt('Ingrese la cyrptomoneda que a comprado: \n1) ETH\n2) BTC\n3) ADA \n4)BNB \n 5)USDT');
-                cantidadComprada= parseFloat('Ingrese la cantidad comprada en unidades');
-                precioCompra = parseFloat( prompt('Ingrese el precio de compra'));
-                fechaCompra = prompt('ingrese la fecha de compra');
-                newInvestment(cryptoComprada, cantidadComprada, precioCompra, fechaCompra);
+                opcionesInversiones = parseInt(prompt('ingrese la opcion que desee:\n1)Agregar inversion \n2)Ver inversiones\n3)Eliminar inversion'));
+                if (opcionesInversiones === 1){
+                cryptoComprada = prompt('Ingrese la cyrptomoneda que a comprado: \n1) BTC\n2) ETH\n3) ADA \n4)BNB \n 5)USDT');
+                cantidadComprada= parseFloat(prompt('Ingrese la cantidad comprada en unidades'));
+                newInvestment(cryptoComprada, cantidadComprada);
+                }else if (opcionesInversiones === 2){
+                    alert(getAllInvestments);
+                }else if (opcionesInversiones === 3){
+
+                }
+
                 break;
             case 4:
                 alert('Gracias por visitarnos!')
@@ -102,7 +118,7 @@
         }
     }
 
-    function getLastID(){
+    function getLastID(){ //--> averiguar mas procesos para saber ID
         return datosUsuarios.length; //--> . length -> la cantidad de usuarios que tengo. 
     }
 
@@ -114,30 +130,43 @@
             contraseña,
             id,
         })
-        return 'Usuario agregado correctamente ' + id; 
+        console.log(datosUsuarios);
+        // return 'Usuario agregado correctamente ' + id; 
     }
 
-    // function getAllUsers(){
-    //     datosUsuarios.forEach((dato)=> console.log(dato.nombre + '-' + dato.mail + '-' + dato.usuario + '-' + dato.contraseña + '-' + id)); //--> "dato" va a ser cada elemento de datosUsuarios en cada iteracion. 
-    // }
     
     function newConversion (moneda, crypto, monto){
         conversionesUsuarios.push({
-            moneda,
-            crypto,
-            monto
+            moneda: monedas[moneda -1],
+            crypto: cryptomonedas [crypto -1],
+            monto,
         })
         
     }
 
-    function newInvestment (cryptoComprada, cantidadComprada, precioCompra, fechaCompra){
-        tusInversiones.push({
-            cryptoComprada,
+    function newInvestment (cryptoComprada, cantidadComprada,){
+        userInvestments.push({
+            cryptoComprada:cryptomonedas[cryptoComprada -1],
             cantidadComprada,
-            precioCompra,
-            fechaCompra,
+           
         })
     }
+
+    // Funcion para imprimr todas las inversiones
+    function getAllInvestments(){
+    userInvestments.forEach((dato)=> console.log(dato.cantidadComprada + '-' + dato.cryptoComprada)); //-->"dato" va a ser cada elemento de userInvestments en cada iteracion (forEach es un bucle que itera x cadaelemento del array). dato. se usa para que no imprima en formato array, si no el dato concreto. 
+    }
+
+    //Funcion para eliminar conversion
+    function deleteConversion(id){
+        conversionesUsuarios = conversionesUsuarios.filter (dato =>dato.id != id);
+    }
+
+    // Funcion para eliminar inversion
+    function deleteInvestment(id){
+        userInvestments = userInvestments.filter (dato => dato.if != id);
+    }
+
 
     function converter(moneda, crypto, monto){
         if ( moneda === 1 && crypto === 1){
