ea22cab1ac454967dfdd714440ee91b3b02b4190
boablock <boablockchain@gmail.com>
2022-09-15 01:13:43 -0300

    funcion y primeros pasos html

--- currency-converter/index.js
+++ currency-converter/index.js
@@ -85,14 +85,15 @@
     const cryptomonedas = [ 'BTC','ETH','ADA','BNB','USDT']
     // console.log(cryptomonedas);
     
-    let option;
-    let precioBtc = 20000;
-    let precioEth = 2000;
-    let dolar = 300;
-    let moneda;
-    let crypto; 
-    let monto;
+    // let option;
+    // let precioBtc = 20000;
+    // let precioEth = 2000;
+    // let dolar = 300;
+    // let moneda;
+    // let crypto; 
+    // let monto;
     // Estrucura inicial, condicional base
+/*
     while (option !== 4){
         option = Number(prompt('Ingrese una opcion:\n 1) Login/sing-up\n 2)Conversor crypto\n 3) Seccion inversiones \n4) Salir'));
         switch(option){
@@ -140,6 +141,62 @@
                 alert('La opcion ingresada no es correcta, intente nuevamente');
         }
     }
+    // funcion para realizar laz conversio
+    function converter(moneda, crypto, monto){
+        if ( moneda === 1 && crypto === 1){
+            return ('$' + monto + ' = '+ monto / precioBtc + 'BTC');
+        }else if (moneda === 1 && crypto === 2){
+            return ('$' + monto + ' = '+ monto / precioEth + 'ETH');
+        } else if (moneda  === 2 && crypto === 1){
+            return ('$' + monto + ' = '+ monto / dolar / precioBtc + 'BTC');
+        }else if (moneda === 2 && crypto === 2){
+            return ('$' + monto + ' = '+ monto / dolar / precioEth + 'ETH');
+        }
+    }
+*/
+
+    // Rehacemos la formula
+    let precioBtc = 20000;
+    let precioEth = 2000;
+    let resultado = 0; //--> en la primer version no tenia variable resultado
+    let monto;
+
+    function conversor(){
+        monto = parseInt(document.querySelector('#monto').value); //--> si esta variable la declaraba afuera, me devolvia NaN. Por que? 
+
+        if (document.querySelector('#USD').checked && document.querySelector('#BTC').checked){
+            return resultado = monto / precioBtc ; // leugo cambiar a return e invocar en el h2 id ='resultado'
+        }else if (document.querySelector('#ARS').checked && document.querySelector('#BTC').checked){
+            return resultado = monto / precioBtc; 
+        }else if (document.querySelector('#USD').checked && document.querySelector('#ETH').checked){
+            return resultado = monto / precioEth; 
+        }else if (document.querySelector('#ARS').checked && document.querySelector('#ETH').checked){
+            return resultado = monto / precioEth; 
+        }else{
+            alert('Tienes que completar todos los inputs');
+        }
+    }
+   
+    //1) traigo el nodo button con id="btnConvertir":
+    const btnConvertir = document.querySelector('#btnConvertir');
+    //2) le aplico un evento al boton y por paramentro le asigno un evento click + la funcion para cuando suceda el evento:
+    btnConvertir.addEventListener('click', ()=> console.log(conversor())); 
+    // console.log(conversor());
+    //3) accedo al nodo padre donde estara mi nuevo elemento
+    const padreResultado = document.querySelector('#padreResultado');
+    //4)creo nodo/ elemento h3
+    agregandoResulado = document.createElement('h3');
+    //4) le agrego el contendio (resultado) con inner.HTML
+    agregandoResulado.innerHTML = resultado; 
+    //5) agregar el nodo HIJO al nodo PADRE
+    document.body.append(agregandoResulado);
+
+
+
+
+
+
+/*
     // funcion para obtener ID
     function getLastIdUsers(){ //--> averiguar mas procesos para saber ID
         return datosUsuarios.length; //--> . length -> la cantidad de usuarios que tengo. 
@@ -183,11 +240,11 @@
             cryptoComprada:cryptomonedas[cryptoComprada -1],//--> cryptoComprada -1 es una posicion de cryptomonedas. 1 de cryptoCompradas equivale a la posicion 0 del array cryptomonedas, de esa forma se hace que coincidan. 
             cantidadComprada,
             id, 
-           
+            
         })
         console.log(userInvestments);
     }
-
+    
     // Funcion para imprimr todas las inversiones
     function getAllInvestments(){
     userInvestments.forEach((dato)=> console.log(dato.cantidadComprada + '-' + dato.cryptoComprada)); 
@@ -201,112 +258,12 @@
         userInvestments = userInvestments.filter (dato => dato.id != id);
     }
 
-    // funcion para realizar laz conversionsz
-    function converter(moneda, crypto, monto){
-        if ( moneda === 1 && crypto === 1){
-            return ('$' + monto + ' = '+ monto / precioBtc + 'BTC');
-        }else if (moneda === 1 && crypto === 2){
-            return ('$' + monto + ' = '+ monto / precioEth + 'ETH');
-        } else if (moneda  === 2 && crypto === 1){
-            return ('$' + monto + ' = '+ monto / dolar / precioBtc + 'BTC');
-        }else if (moneda === 2 && crypto === 2){
-            return ('$' + monto + ' = '+ monto / dolar / precioEth + 'ETH');
-        }
-    }
-  
   
 
 
     // const suma = (a,b) => a / b; 
     // alert(suma(monto),precioBtc); 
-
-
-    // // MASTER
-    // let option;
-    // const datosUsuario =[
-    //     {
-    //         // id:1,
-    //         nombre:'Pedro',
-    //         mail:'pedro_montero@gmail.com',
-    //         userName: 'Pepemonte1',
-    //         passWord: 'Pepe123',
-    //     }
-    // ]
-    // console.log(datosUsuario);
-
-
-    // let precioBtc = 20000;
-    // let precioEth = 2000;
-    // let dolar = 300;
-  
-    // let moneda;
-    // let crypto; 
-    // let monto;
-
-    // while (option !== 4){
-    //     option = Number(prompt('Ingrese una opcion:\n 1) Login/sing-up\n 2)Conversor crypto\n 3)Seccion favoritos\n 4) Salir'));
-    //     switch(option){
-    //         case 1:
-    //             const nombre = prompt('Ingresa tu nombre');
-    //             const mail = prompt('Ingresa tu mail');
-    //             const userName = prompt('Ingresa tu usuario');
-    //             const passWord =prompt ('Ingresa tu contraseña')
-    //             // const id = getLastID() + 1;
-    //             newUser(nombre, mail, userName, passWord);
-    //             alert('Bienvenido ' + nombre);
-    //             break;
-    //         case 2:
-    //             moneda = parseInt(prompt('ingrese un numero segun sea su moneda: \n 1)USD 2)ARS'));
-    //             crypto = parseInt(prompt('Ingrese un numero segun la criptomoneda deseada: \n1)BTC \n2)ETH'));
-    //             monto = parseInt(prompt('Ingrese el monto que desee convertir'));
-    //             alert(converter(moneda, crypto, monto));
-    //             break;
-    //         case 3:
-    //             alert('Ingresaste la opcion 3');
-    //             break;
-    //         case 4:
-    //             alert('Ingresaste la opcion 4');
-    //             break;
-    //         default:
-    //             alert('La opcion ingresada no es correcta, intente nuevamente');
-    //     }
-    // }
-
-
-
-    // function newUser (nombre, mail, usuario, contraseña){
-    //     datosUsuario.push({
-    //         nombre,
-    //         mail,
-    //         usuario,
-    //         contraseña,
-    //     })
-    // }
-    
-
-    // function converter(moneda, crypto, monto){
-    //     if ( moneda === 1 && crypto === 1){
-    //         return ('$' + monto + ' = '+ monto / precioBtc + 'BTC');
-    //     }else if (moneda === 1 && crypto === 2){
-    //         return ('$' + monto + ' = '+ monto / precioEth + 'ETH');
-    //     } else if (moneda  === 2 && monto === 1){
-    //         return ('$' + monto + ' = '+ monto / dolar / precioBtc + 'BTC');
-    //     }else if (moneda === 2 && monto === 2){
-    //         return ('$' + monto + ' = '+ monto / dolar / precioEth + 'ETH');
-    //     }
-    // }
-  
-  
-
-
-    // // const suma = (a,b) => a / b; 
-    // // alert(suma(monto),precioBtc); 
-
-    // // function suma (a,b){
-    // //     return a/b; 
-    // // }
-
-    // // console.log(suma(100,10));
+*/
 
 
 
