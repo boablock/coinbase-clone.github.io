51f2751ce5a47e6b98fe4d23cfb546dee3e634c6
boablock <boablockchain@gmail.com>
2022-09-17 17:18:07 -0300

    .

--- currency-converter/js/converter.js
+++ currency-converter/js/converter.js
@@ -35,3 +35,93 @@ btnConvertir.addEventListener("click", () => {
 
 document.body.append(agregandoResultado);
 
+
+
+
+
+
+
+    // MASTER
+    
+    // Array para recolectar los datos de cada usuario
+    
+    const datosUsuarios =[
+      {
+          id:1,
+          nombre:'Pedro',
+          apellido: 'Perez',
+          mail:'pedro_montero@gmail.com',
+          usuario: 'Pepemonte1',
+          contraseña: 'Btc1230',
+          cryptoFav: 'BTC',
+      },
+      {
+          id:2,
+          nombre:'Juan',
+          apellido: 'Gonzales',
+          mail:'juan_gonzales@gmail.com',
+          usuario: 'Juaneth1',
+          contraseña: 'Ethereum293',
+          cryptoFav: 'ETH',
+      },
+      {
+          id:3,
+          nombre:'Manuel',
+          apellido: 'Conde',
+          mail:'manu_conde@gmail.com', 
+          usuario: 'Manueht12',
+          contraseña: 'Ethereum999',
+          cryptoFav: 'ETH',
+      }
+  ]
+  // console.log(datosUsuarios);
+
+  //Metodo filter datosUsuarios.cryptoFav
+  let userCryptoFav = datosUsuarios.filter(function(datosUsuarios){return datosUsuarios.cryptoFav === 'ETH';})
+  console.log(userCryptoFav);
+
+  //Metodo filter datosUsuarios.apellido
+  const busqueda = datosUsuarios.filter(dato =>{return dato.apellido  === 'Gonzales';})
+  // console.log(busqueda);
+
+  // Array para recolectar las conversiones de cada usuario
+  const conversionesUsuarios =[
+      {   
+          id:1, 
+          moneda: 1,
+          crypto:1,
+          monto: 10000, 
+      },
+      {
+          id:2,
+          moneda:2,
+          crypto:1,
+          monto: 100, 
+      }
+  ]
+  // console.log(conversionesUsuarios);
+ 
+  //Array inversiones de usuarios
+  let userInvestments =[
+      {
+          cryptoComprada: 'BTC',
+          cantidadComprada: 0.01, 
+          id:1,
+      },
+      {
+          cryptoComprada:'ADA',
+          cantidadComprada: 100,
+          id:2,
+      }
+      
+  ]
+  // console.log(userInvestments);
+
+  // Metodo filter userInvestments.cryptoComprada (HACER)
+
+  //Array monedas
+  const monedas = ['USD','ARS',]
+
+  //Array cryptomonedas
+  const cryptomonedas = [ 'BTC','ETH','ADA','BNB','USDT']
+  // console.log(cryptomonedas);
