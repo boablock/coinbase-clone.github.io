4a61ed943989a1f13616d750cf3bf138e3fa81c0
boablock <boablockchain@gmail.com>
2022-09-26 10:57:27 -0300

    listado de seguimiento renderizado sin boton borrar

--- currency-converter/assets/js/lista-seguimiento.js
+++ currency-converter/assets/js/lista-seguimiento.js
@@ -11,6 +11,7 @@
  * b-crear array vacio de listaDeSeguimineto 
  * c-creando luego la funcion para agregar esa moneda a la listaDeSeguimiento (function agregaraListaDeSeguimiento, con push al array) 
  * d-invocar la funcion push dentro de la funcion de eventoBtnAgregar
+ * 4) Crear funcion para renderizar la lista de seguimiento en el sitio
  * 
  */
 
@@ -21,55 +22,62 @@ const monedas = [
 {
     nombre: 'BTC',
     precio: 20000,
+    marketcap: 20000000,
     id: 1,
 },
 {
     nombre: 'ETH',
     precio: 1000,
+    marketcap: 10000000,
     id:2,
 },
 {
     nombre: 'USDT',
     precio: 1,
+    marketcap: 20000000,
     id:3,
 },
 {
     nombre: 'ADA',
     precio: 2,
+    marketcap: 600000,
     id:4,
 },
 {
     nombre: 'BNB',
     precio: 400,
+    marketcap: 300000,
     id:5,
 },
 ];
 
-const bntListaMonedas = document.querySelector('#btn-lista-monedas'); 
-const divListaSeguimiento= document.querySelector('#div-lista-seguimiento');
-let listaDeSeguimiento =[];
+const btnDesplegarListaMonedas = document.querySelector('#btn-lista-monedas'); 
+const divListadoMonedas= document.querySelector('#div-listado-monedas');
+const divListaDeSeguimiento= document.querySelector('#div-lista-de-seguimiento');
+//3)b-
+let listaDeSeguimiento =[]; //-> ERROR: me muestra solo la primer moneda seleccionada 
 
 
 // 1)
 function crearListadoMonedas(){
     monedas.forEach(moneda=>{
-        divListaSeguimiento.innerHTML += ` 
+        divListadoMonedas.innerHTML += ` 
         <div>
             <h4>${moneda.nombre}</4>
             <button class='btnAgregaraSeguimiento' id='btn-agregar${moneda.id}'>Agregar</button>
         </div> 
         `
     })
-    EventoBntAgregar();
+    EventoBtnAgregar();
 }
 
 //2)
-bntListaMonedas.addEventListener('click',()=>{
+btnDesplegarListaMonedas.addEventListener('click',()=>{
     crearListadoMonedas();
 })
 
-//3)
-function EventoBntAgregar(){
+//3)a-
+function EventoBtnAgregar(){
     monedas.forEach(moneda=>{
         document.querySelector(`#btn-agregar${moneda.id}`).addEventListener('click',()=>{
             agregaraListaDeSeguimiento(moneda);
@@ -77,20 +85,36 @@ function EventoBntAgregar(){
 
     })
 }
+//3)c- (VER SI FUNCIONA EL PUSH)
 function agregaraListaDeSeguimiento(moneda){
-listaDeSeguimiento.push(moneda);
-console.log(listaDeSeguimiento);
-renderizarListaDeSeguimiento();
+    let existe = listaDeSeguimiento.some(mon=>mon.id === moneda.id);
+    if (existe === false){ //-> la moneda va a entrar a la lista de seg por este if solo si no existiese la moneda en el array. 
+        moneda.cantidad = 1; //-> propiedad creada en el ejemplo del after
+        listaDeSeguimiento.push(moneda);
+    }
+    else{
+        let monFind = listaDeSeguimiento.find(mon => mon.id === moneda.id) // 
+        monFind.cantidad++;
+    }
+    console.log(listaDeSeguimiento);
+    renderizarListaDeSeguimiento(); //-->voy a querer que se renderice la lista cada vez que se agregue un producto a la misma (al array listaDeSeguimietno).
 }
-
+//4)
+//-> con esta funcion deja de pushear.
 function renderizarListaDeSeguimiento(){
+    divListaDeSeguimiento.innerHTML ='';
     listaDeSeguimiento.forEach(moneda=>{
-        divListaSeguimiento.innerHTML += ` 
-        <div>
-            <h4>${moneda.nombre}</4>
-            <p> $${moneda.precio}</p>
-            <button class='btnBorrar' id='btn-borrar${moneda.id}'>Borrar</button>
-        </div> 
-        `
+        let li = document.createElement('li');
+        li.innerHTML += ` 
+        <hr> ${moneda.nombre} - Precio: $${moneda.precio} - Market cap: ${moneda.marketcap} - <button class='btnBorrar' id='btn-borrar${moneda.id}'>Borrar</button> `;
+        divListaDeSeguimiento.appendChild(li);
+        
+        // divListaDeSeguimiento.innerHTML += `<div>
+        //     <h4>${moneda.nombre}</4>
+        //     <p>Precio: $${moneda.precio}</p>
+        //     <p>Market cap: ${moneda.marketcap}</p>
+        //     <p>Cantidad: ${moneda.cantidad}</p>
+        //     <button class='btnBorrar' id='btn-borrar${moneda.id}'>Borrar</button>
+        // </div> `
     })
 }
