e624763d1898fb94bdfbcd2f84581fe9e62d0d0e
boablock <boablockchain@gmail.com>
2022-09-28 18:45:31 -0300

    function saveUserData working with local storage

--- currency-converter/assets/js/login.js
+++ currency-converter/assets/js/login.js
@@ -1,1 +1,30 @@
 
+
+
+document.getElementById('form-user').addEventListener('submit', saveUserData);
+
+function saveUserData(e){
+    let email = document.getElementById('input-email').value; 
+    let password = document.getElementById('input-pass').value;
+    // console.log(email, password);
+
+    const userData = {
+        email,
+        password,
+    }; 
+   
+
+   if(localStorage.getItem('userDataArray') === null){
+    let userDataArray = [];
+    userDataArray.push(userData);
+    localStorage.setItem('userDataArray', JSON.stringify(userDataArray));//-> LO CARGO
+   }else{
+    let userDataArray = JSON.parse(localStorage.getItem('userDataArray'));
+    userDataArray.push(userData); //-> LO ACTUALIZO
+    localStorage.setItem('userDataArray', JSON.stringify (userDataArray));
+
+   }
+
+   e.preventDefault();
+}
+
