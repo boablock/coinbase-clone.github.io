5c707c7b9264d9eadaa724b50d72cbd312cfb097
boablock <boablockchain@gmail.com>
2022-10-21 11:23:20 -0300

    working on amount converter

--- currency-converter/assets/js/converter.js
+++ currency-converter/assets/js/converter.js
@@ -4,32 +4,34 @@ const moneda = document.querySelector("#moneda");
 const criptomoneda = document.querySelector("#criptomonedas");
 const formContainer = document.querySelector(".form-side");
 const containerAnswer = document.querySelector(".container-answer");
+const amount = Number(document.querySelector("#amount")).value;
 
 const objBusqueda = { //-> el evento "change" seteara este objeto , evento que tiene asociada (se llamo) a la funcion getValue
     moneda: '',
-    criptomoneda: ''
+    criptomoneda: '',
+    amount:parseFloat('')
 }
 
-document.addEventListener('DOMContentLoaded', () => {
     consultarCriptos();
 
     form.addEventListener('submit', submitForm);
+    amount.addEventListener('input', getValue);
     moneda.addEventListener('change', getValue); //-> the e.target.name of the getValue f(x) will be 'moneda'
     criptomoneda.addEventListener('change', getValue);//-> e.target.name of the getValue f(x) will be 'criptomoneda', and the value will be the user selection: objBusqueda[e.target.name] = e.target.value;
     
-})
-
 function submitForm(e){
     e.preventDefault();
-    const {moneda, criptomoneda} = objBusqueda;
-    if (moneda === '' || criptomoneda === '') {
-        showError('Seleccione ambas monedas...');
+    const {moneda, criptomoneda, amount} = objBusqueda;
+    if (moneda === '' || criptomoneda === '' || amount === '') {
+        showError('Complete todos los campos');
         return;
     }
     consultarAPI(moneda, criptomoneda);
     //console.log(moneda);
     //console.log(criptomoneda);
+    console.log(amount);
 }
+
 
 function consultarAPI(moneda, criptomoneda){
     const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
@@ -37,17 +39,21 @@ function consultarAPI(moneda, criptomoneda){
         .then(resultado => resultado.json())
         .then(resultadoJson => {
             mostrarCotizacion(resultadoJson.DISPLAY[criptomoneda][moneda]); //-> data response setting
-            //console.log(resultadoJson.DISPLAY[criptomoneda][moneda]);
+            console.log(resultadoJson.DISPLAY[criptomoneda][moneda]);
         })
         .catch(error => console.log(error));
 }
 
 function mostrarCotizacion(data){
     clearHTML();
+    const {amount} = objBusqueda;
+    
     const {PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, LASTUPDATE} = data;
     const answer = document.createElement('div');
     answer.classList.add('display-info');
+    let resultado =  parseFloat(amount * PRICE);
     answer.innerHTML = `
+        <p class="main-price">Resultado: <span>${resultado}</span></p>
         <p class="main-price">Precio: <span>${PRICE}</span></p>
         <p>Precio más alto del día:: <span>${HIGHDAY}</span></p>
         <p>Precio más bajo del día: <span>${LOWDAY}</span></p>
@@ -68,7 +74,7 @@ function showError(mensage){
     setTimeout(() => error.remove(), 3000);
 }
 
-function getValue(e){ //-> here you will get the value of the select where de function getValue is called. And then, both values (both selections, moneda y cryptomoneda) will be stored in the 'ObjBusqueda'.
+function getValue(e){ //-> here you will get the value of the select where the function getValue is called. And then, both values (both selections, moneda y cryptomoneda) will be stored in the 'ObjBusqueda'.
     objBusqueda[e.target.name] = e.target.value;
     console.log(e.target.name); 
 }
